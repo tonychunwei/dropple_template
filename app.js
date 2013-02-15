1647ee8364dda7e0c5c0f87ba54107315be11ea5
jaclync <watertranquil@gmail.com>
2013-02-14 22:40:35 -0800

    app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 8080);
-  app.set('views', __dirname + '/views');
+  app.set('views', __dirname + '/public');
   app.set('view engine', 'jade');
 /*
 app.set('views', __dirname + '/views');
